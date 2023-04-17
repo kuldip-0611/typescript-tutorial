01cc6b15c57f98250871952d9055b9b391268eaf
kuldip-zignuts <kuldipp@zignuts.com>
2023-04-17 14:42:57 +0530

    literal in typescript

--- test.ts
+++ test.ts
@@ -1,12 +1,14 @@
-const add = (a:number | string ,b : number | string) =>{
-    if(typeof(a) === 'number' && typeof(b)==='number'){
-        return a+b;
+const add = (a:number | string ,b : number | string,type:'as-string' | 'as-number') =>{
+    if(type='as-number'){
+        return (+a)+(+b);
     }
     else{
         return a.toString() + b.toString()
     }
 }
 
-console.log(add('kuldip','pandya'))
+console.log(add('kuldip','pandya','as-string'))
 
-console.log(add(23,45))
+console.log(add(23,45,'as-number'))
+
+let type1 : 'as-number' | 'as-string' ='as-number'
