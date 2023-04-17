4ffd1d9975483a212c9f607b546cfad6ef32d24a
kuldip-zignuts <kuldipp@zignuts.com>
2023-04-17 12:10:32 +0530

    type inference

--- test.ts
+++ test.ts
@@ -1,5 +1,20 @@
-function add(a:number,b:number){
-    return a+b
-}
+// function add(a:number,b:number){
+//     return a+b
+// }
 
-console.log(add(12,34))
+// console.log(add(12,34))
+
+//--->type inference
+
+const data = 'abc' //by default type string
+
+//can't assign a number or any other type
+
+let x; //by default type is any and can assign any value
+
+x=12;
+x=34;
+x='djfk';
+x=true
+
+let y:5 //y have only value 5 can not assign any other value or type
