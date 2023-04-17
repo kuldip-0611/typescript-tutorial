6d1f9347b15edc4415fbde35f884d3f853930411
kuldip-zignuts <kuldipp@zignuts.com>
2023-04-17 12:28:11 +0530

    define type for object

--- test.ts
+++ test.ts
@@ -1,6 +1,13 @@
-const user:{name:string,age:number} ={
+type objType ={name:string,age:number;}
+
+const user:objType ={
     name:'kuldip',
     age:21
+}
+
+const data:objType = {
+    name:'peter',
+    age:23
 }
 
 console.log(user.name) //can't access the name we have to provide the type
